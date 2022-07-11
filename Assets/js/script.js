0b7f056bb11bee5e64b2d4bde80bdd927321c387
PeachDoors <taylorpterrill@gmail.com>
2022-07-10 20:28:43 -0600

    name form

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,6 +3,11 @@ var nextButton = document.getElementById('next-button');
 var questionContainer = document.getElementById('question-container');
 var questionElement = document.getElementById('question');
 var answerButtonsElement = document.getElementById('answer-buttons');
+var nameContainer = document.getElementById('name-container')
+var finalScore = document.getElementById('final-score');
+
+var timerEl = document.getElementById('timer');
+var timeLeft = 100;
 
 var shuffledQuestions;
 var questionIndex;
@@ -28,9 +33,9 @@ var questions = [
         ]
     },
     { 
-        question: 'How would you write "Hello World" in an alert box?',
+        question: 'How would you display "Hello World" in an alert box?',
         answers: [
-            { text: 'console.log("Hello, World")', correct: false },
+            { text: 'windowAlert("Hello, World")', correct: false },
             { text: 'alert("Hellow World")', correct: true },
             { text: 'myFunction("Hello World")', correct: false },
             { text: '<div id="Alert">Hello World</div>', correct: false },
@@ -51,7 +56,8 @@ function startQuiz() {
     shuffledQuestions = questions.sort(() => Math.random() - .5);
     questionIndex = 0;
 
-    nextQuestion()
+    setTime();
+    nextQuestion();
 
 }
 
@@ -68,12 +74,6 @@ function resetState() {
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     }
-}
-
-// clears status
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
 }
 
 function showQuestion(question) {
@@ -94,18 +94,6 @@ function showQuestion(question) {
 
 }
 
-// creates functionality for next question button
-nextButton.addEventListener('click', () => {
-    questionIndex++;
-    nextQuestion();
-})
-
-
-
-
-
-
-
 //function to pick answer
 function selectAnswer(e) {
     var selectedButton = e.target;
@@ -117,20 +105,63 @@ function selectAnswer(e) {
     if (shuffledQuestions.length > questionIndex + 1) {
         nextButton.classList.remove('hide');
     } else {
-        //show scoreboard
+        showNameForm();
     }
     
 }
+
+// clears status
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+// creates functionality for next question button
+nextButton.addEventListener('click', () => {
+    questionIndex++;
+    nextQuestion();
+})
 
 //decides if status is correct or wrong
 function setStatusClass (element, correct)  {
     clearStatusClass(element)
     if (correct) {
         element.classList.add('correct');
-        // document.getElementById("answer-buttons").disabled = ture;
     } else {
         element.classList.add('wrong');
-        // document.getElementById("answer-buttons").disabled = ture;
+        loseTime();
     }
 }
+
+// timer functionality
+function setTime() {
+    var timerInterval = setInterval(function() {
+        timeLeft--;
+        timerEl.textContent = timeLeft;
+
+        if(timeLeft <= 0) {
+            clearInterval(timerInterval);
+            showNameForm();
+        }
+    }, 1000);
+}
+
+// subtracts time when incorrect answer is clicked
+function loseTime() {
+    timeLeft = (timeLeft - 2);
+}
+
+
+function showNameForm() {
+    // stop timer - set scoreboard-timer = timeLeft
+    console.log("s")
+    resetState();
+    finalScore.innerText = timeLeft;
+    questionContainer.classList.add('hide');
+    nameContainer.classList.remove('hide');
+    // log time left
+
+    // show scoreboard
+}
+
 
